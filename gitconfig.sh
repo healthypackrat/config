dbf7f281ebaa2963f5bdf4424b1ff47edc65b5b8
healthypackrat <healthypackrat@gmail.com>
2018-09-28 19:23:37 +0900

    Add git aliases

--- gitconfig.sh
+++ gitconfig.sh
@@ -11,4 +11,8 @@ gitconfig color.ui auto
 gitconfig alias.b branch
 gitconfig alias.ci commit
 gitconfig alias.co checkout
+gitconfig alias.d diff
+gitconfig alias.ds 'diff --cached'
+gitconfig alias.l log
+gitconfig alias.ld "log --format=tformat:'%h [%cD] %s'"
 gitconfig alias.st status
