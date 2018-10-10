3472b751594eaa26a4baecc21803f6e2398fff2e
healthypackrat <healthypackrat@gmail.com>
2018-10-10 22:48:18 +0900

    Update git aliases

--- gitconfig.sh
+++ gitconfig.sh
@@ -11,9 +11,9 @@ gitconfig color.ui auto
 gitconfig alias.b branch
 gitconfig alias.ci commit
 gitconfig alias.co checkout
-gitconfig alias.d diff
 gitconfig alias.ds 'diff --cached'
-gitconfig alias.l log
 gitconfig alias.ld "log --format=tformat:'%h [%aD] %s'"
 gitconfig alias.ln 'log --name-status'
+gitconfig alias.lp 'log -p'
+gitconfig alias.lso 'ls-files -o'
 gitconfig alias.st status
